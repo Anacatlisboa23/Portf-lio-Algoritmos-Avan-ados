a0c351fa638183ffabf0c313f418b8663e682cfe
maribrag <120012498+maribrag@users.noreply.github.com>
2023-03-07 20:31:08 +0000

    Update Motiffinding.py

--- Motiffinding.py
+++ Motiffinding.py
@@ -144,108 +144,50 @@ class MotifFinding:
                 s = self.nextVertex(s)
         return melhorMotif
     
-    #test consensus (heuristic)
-    def test_heuristic_consensus(self):
-        motf = MotifFinding()
-        motf.readFile("exemploMotifs.txt","dna")
-        self.assertEqual(motf.heuristicConsensus(), [0, 6, 9, 27, 3])
-        motf = MotifFinding()
-        motf.readFile("exemploMotifs2.txt","dna")
-        self.assertEqual(motf.heuristicConsensus(), [26, 4, 45, 63, 51])
-
-    # Consensus (heuristic)
-    def heuristicConsensus(self)-> list:
-        '''
-            Este algoritmo apresenta um elevado nº de soluções parciais em cada iteração.Selecionando o melhor resultado no final
-           
-       '''
-        for y in range(len(self.seqs)):              # Considera apenas as 2 primeiras sequências 
-            seque=(self.seqSize(y), self.seqs[0:2])  
-            restante = self.seqs[2:] 
-            ExhS= seque.exhaustiveSearch()           #Maximiza a função de avaliação(score)
-        for i in range(len(restante)):               #Procura posições de motifs para as restantes posições
-            aux = ExhS
-            melhorScore = -1
-            for h in range(len(restante[i])-self.motifsize):   #Para cada uma das restantes sequências,de maneira iterativa, escolher a melhor posição inicial na sequência, de modo a maximizar o score, considerando as posições anteriores fixas
-                aux.append(h)
-                score = self.score(aux)         
-                if (score > bestScore):
-                    bestScore = score
-                    best_i = h
-                aux.pop()
-            ExhS.append(best_i)
-        return ExhS
-    
-#test heuristic stochastic
-    def test_heuristic_stochastic(self):
-        moti= MotifFinding()
-        moti.readFile("exemploMotifs.txt","dna")
-        self.assertEqual(type(moti.heuristicStochastic()), list)
-    
-    # Consensus (heuristic)
-
-    def heuristicStochastic(self)-> list:
-        from random import randint
-        from random import random
-        '''
-            Este algoritmo depende bastante das posições iniciais. Os resultados 
-            podem ser melhorados se considerarmos várias ordens de apresentação das 
-            sequências distintas.
-            '''
-        best_Score_list = []
-        for x in range(1000): #Inicia com valores aleatórios todas as posições
-            startpos = [random.randint(0, self.seqSize(x)-self.motifSize) for x in range(len(self.seqs))]
-            motif = self.createMotifFromIndexes(startpos) #Perfil construido com todas as posições iniciais
-            motif.createPWM()
-            score = self.scoreMult(startpos)
-            novo_score = score + 0.000001
-            while score < novo_score:
-                for i in range(len(startpos)): #Avalia a melhor posição inicial para cada sequência baseando-se no perfil
-                    startpos[i] = motif.mostProbableSeq(self.seqs[i])
-                score = novo_score #Aqui vamos verificar se houve alguma melhoria
-                novo_score = self.scoreMult(startpos)
-                motif = self.createMotifFromIndexes(startpos)
-                motif.createPWM()
-            best_Score_list.append(novo_score)
-
-        return best_Score_list #Lista com os scores melhores encontrados
-    
-    # Gibbs sampling
-    #test:
-    from unittest import TestCase
-    from MySeq import MySeq
-
-    def test_gibbs(self):
-        motif = MotifFinding()
-        motif.readFile("exemploMotifs.txt","dna")
-        self.assertEqual(type(motif.gibbs()), list)
-
-        # Gibbs sampling 
-        '''
-            O algoritmo anteriormente demostrado pode ser melhorado introduzindo o método de Gibbs Sampling,
-            sendo este mais lento, escolhendo novos segmentos de forma aleatória, aumentando assim as possibilidades
-            de convergir para uma solução correta.
-            '''
         
 
-    def gibbs (self, iteration=100): 
+        
+        # Gibbs sampling 
+
+
+    def gibbs (self, iteration : int =100) -> list[int]: 
+        """
+            Algoritmo que implementa o  Gibbs Sampling, iniciando com subsequências seleccionadas aleatoriamente, 
+            sendo posteriormente pontuadas contra o modelo inicial. 
+            Em cada iteração, o algoritmo efectua uma pesquisa local, 
+            decidindo probabilisticamente se um dos motifs deve ser actualizado.
+
+        Argumento: 
+            iteration: nº de iterações
+        Devolve:
+             lista de Motifs 
+
+        """
         from random import randint
         from random import random
-        startpos = [random.randint(0, self.seqSize(x)-self.motifSize) for x in range(len(self.seqs))] #Escolhe posições iniciais de forma aleatória
+
+         #Escolher posições iniciais de forma aleatória
+        startpos = [random.randint(0, self.seqSize(x)-self.motifSize)
+                    for x in range(len(self.seqs))]
         bestMotifs = startpos[:]
         bestMotifsScore=self.scoreMult(startpos)
-        time_last_improvement=0  # Até enquanto der para encontrar scores melhores
+        time_last_improvement=0 
+
         while time_last_improvement< iteration:
              time_last_improvement += 1
-             for x in range(iteration): #Escolhe aleatoriamente uma sequência 
-                i = random.randite(0,len(self.seqs)-1) #Posição
+
+             #Escolher aleatoriamente uma sequência 
+             for x in range(iteration):  
+                i = random.randint(0,len(self.seqs)-1)
                 startpos.pop(i)
-                outraseq=self.seqs.pop(i) #Indica qual a posição da sequência que foi retirada
+                outraseq=self.seqs.pop(i) 
                 perfil=self.createMotifFromIndexes(startpos) 
-                perfil.create_PWM() #Perfil P das sequências s
-                self.seqs.insert(i,outraseq) #Adiciona a sequência que foi removida anteriormente
-                probabilidades=perfil.probAllPositions(self.seqs[outraseq]) # Seleciona a posição baseada na roullette
-                roul=self.roulette(probabilidades) #Cria a roleta de probabilidades
+                perfil.create_PWM() 
+                self.seqs.insert(i,outraseq) 
+
+                #vamos ver a probabilidade das subsquencias possiveis na sequencia que foi removida
+                probabilidades=perfil.probAllPositions(self.seqs[outraseq]) 
+                roul=self.roulette(probabilidades) 
                 startpos.insert(i,roul)
                 score= self.scoreMult(startpos)
                 if score> bestMotifsScore: 
@@ -253,6 +195,7 @@ class MotifFinding:
                     bestMotifs = list(startpos)
                     time_last_improvement=0
         return bestMotifs
+
 
     def roulette(self, f):
         from random import random
@@ -267,59 +210,69 @@ class MotifFinding:
         return ind-1
 
 # tests
+import unittest
+from unittest import TestCase
+from MySeq import MySeq
 
-def test1():  
-    sm = MotifFinding()
-    sm.readFile("exemploMotifs.txt","dna")
-    sol = [25,20,2,55,59]
-    sa = sm.score(sol)
-    print(sa)
-    scm = sm.scoreMult(sol)
-    print(scm)
+class TestStringMethods(unittest.TestCase):
+    def test_gibbs(self):
+        motif = MotifFinding()
+        motif.readFile("exemploMotifs.txt","dna")
+        self.assertEqual(type(motif.gibbs()), list)
 
-def test2():
-    print ("Test exhaustive:")
-    seq1 = MySeq("ATAGAGCTGA","dna")
-    seq2 = MySeq("ACGTAGATGA","dna")
-    seq3 = MySeq("AAGATAGGGG","dna")
-    mf = MotifFinding(3, [seq1,seq2,seq3])
-    sol = mf.exhaustiveSearch()
-    print ("Solution", sol)
-    print ("Score: ", mf.score(sol))
-    print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
+    def test1():  
+        sm = MotifFinding()
+        sm.readFile("exemploMotifs.txt","dna")
+        sol = [25,20,2,55,59]
+        sa = sm.score(sol)
+        print(sa)
+        scm = sm.scoreMult(sol)
+        print(scm)
 
-    print ("Branch and Bound:")
-    sol2 = mf.branchAndBound()
-    print ("Solution: " , sol2)
-    print ("Score:" , mf.score(sol2))
-    print("Consensus:", mf.createMotifFromIndexes(sol2).consensus())
+    def test2():
+        print ("Test exhaustive:")
+        seq1 = MySeq("ATAGAGCTGA","dna")
+        seq2 = MySeq("ACGTAGATGA","dna")
+        seq3 = MySeq("AAGATAGGGG","dna")
+        mf = MotifFinding(3, [seq1,seq2,seq3])
+        sol = mf.exhaustiveSearch()
+        print ("Solution", sol)
+        print ("Score: ", mf.score(sol))
+        print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
+
+        print ("Branch and Bound:")
+        sol2 = mf.branchAndBound()
+        print ("Solution: " , sol2)
+        print ("Score:" , mf.score(sol2))
+        print("Consensus:", mf.createMotifFromIndexes(sol2).consensus())
     
-    print ("Heuristic consensus: ")
-    sol1 = mf.heuristicConsensus()
-    print ("Solution: " , sol1)
-    print ("Score:" , mf.score(sol1))
+        print ("Heuristic consensus: ")
+        sol1 = mf.heuristicConsensus()
+        print ("Solution: " , sol1)
+        print ("Score:" , mf.score(sol1))
 
-def test3():
-    mf = MotifFinding()
-    mf.readFile("exemploMotifs.txt","dna")
-    print ("Branch and Bound:")
-    sol = mf.branchAndBound()
-    print ("Solution: " , sol)
-    print ("Score:" , mf.score(sol))
-    print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
+    def test3():
+        mf = MotifFinding()
+        mf.readFile("exemploMotifs.txt","dna")
+        print ("Branch and Bound:")
+        sol = mf.branchAndBound()
+        print ("Solution: " , sol)
+        print ("Score:" , mf.score(sol))
+        print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
 
-def test4():
-    mf = MotifFinding()
-    mf.readFile("exemploMotifs.txt","dna")
-    print("Heuristic stochastic")
-    sol = mf.heuristicStochastic()
-    print ("Solution: " , sol)
-    print ("Score:" , mf.score(sol))
-    print ("Score mult:" , mf.scoreMult(sol))
-    print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
+    def test4():
+        mf = MotifFinding()
+        mf.readFile("exemploMotifs.txt","dna")
+        print("Heuristic stochastic")
+        sol = mf.heuristicStochastic()
+        print ("Solution: " , sol)
+        print ("Score:" , mf.score(sol))
+        print ("Score mult:" , mf.scoreMult(sol))
+        print("Consensus:", mf.createMotifFromIndexes(sol).consensus())
     
-    sol2 = mf.gibbs(1000)
-    print ("Score:" , mf.score(sol2))
-    print ("Score mult:" , mf.scoreMult(sol2))
+        sol2 = mf.gibbs(1000)
+        print ("Score:" , mf.score(sol2))
+        print ("Score mult:" , mf.scoreMult(sol2))
 
-#test4()
+if _name_ == '_main_':
+    unittest.main()
